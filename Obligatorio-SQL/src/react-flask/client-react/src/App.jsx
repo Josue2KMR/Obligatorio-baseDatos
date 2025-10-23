2afc30803b7a191cc18dbb6c2a6f709a6108e1c2
Josué Merino <josue.merino@correo.ucu.edu.uy>
2025-10-23 12:08:17 -0300

    Conexion de la base de datos con python y react

--- Obligatorio-SQL/src/react-flask/client-react/src/App.jsx
+++ Obligatorio-SQL/src/react-flask/client-react/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  useEffect(() => {
+  fetch("http://localhost:5000/api/reservas")
+    .then(res => res.json())
+    .then(data => console.log(data));
+}, []);
   const [count, setCount] = useState(0)
 
   return (
--- Obligatorio-SQL/src/react-flask/server-flask/main.py
+++ Obligatorio-SQL/src/react-flask/server-flask/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app) # Enable CORS for all routes
@@ -9,6 +10,30 @@ CORS(app) # Enable CORS for all routes
 def hello_world():
     return 'Hello World'
 
+@app.route('/api/reservas')
+def get_reservas():
+    # Connect to the database
+    try:
+        cnx = mysql.connector.connect(
+            user='root',
+            password='rootpassword',
+            host='localhost',
+            database='obligatorio')
+
+        cursor = cnx.cursor()
+        print("Conexion exitosa")
+    except mysql.connector.Error as err:
+        print("Error de conexion: {}".format(err))
+        return
+    # Execute a query
+    cursor.execute("SELECT * FROM participante")
+    results = cursor.fetchall()
+
+
+    cursor.close()
+    cnx.close()
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
